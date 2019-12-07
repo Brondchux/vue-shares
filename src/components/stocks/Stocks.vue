bd561bcbd84b3c3b3ba905b7571548957c3439f1
Brondchux <brondchux@gmail.com>
2019-12-07 14:15:27 -0500

    Added Vuex

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,19 +8,15 @@
 
 <script>
 import Stock from './Stock.vue';
+
 export default {
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: 'Google', price: 98 },
-                { id: 2, name: 'Afrihub', price: 63 },
-                { id: 3, name: 'Yelp', price: 78 },
-                { id: 4, name: 'Linkedin', price: 23 }
-            ]
-        }
-    },
     components: {
         appStock: Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks
+        }
     }
 }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'Google', price: 98 },
+    { id: 2, name: 'Afrihub', price: 63 },
+    { id: 3, name: 'Yelp', price: 78 },
+    { id: 4, name: 'Linkedin', price: 23 }
+]
