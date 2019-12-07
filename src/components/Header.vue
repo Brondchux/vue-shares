bf04df78d9a34c6b21ebcb59f0bdaba288f0ac32
Brondchux <brondchux@gmail.com>
2019-12-07 11:39:40 -0500

    Added Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Header</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info mb-5">
+        <router-link to="/" tag="a" class="navbar-brand" active-class="active">Stock Trader</router-link>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <router-link to="/portfolio" tag="li" class="nav-item" active-class="active"><a class="nav-link">Portfolio</a></router-link>
+                <router-link to="/stocks" tag="li" class="nav-item" active-class="active"><a class="nav-link">Stocks</a></router-link>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" @click="endDay" style="cursor: pointer">End Day</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a 
+                        class="nav-link dropdown-toggle" 
+                        href="#" id="navbarDropdown" 
+                        role="button" 
+                        data-toggle="dropdown" 
+                        aria-haspopup="true" 
+                        aria-expanded="false">
+                        Save &amp; Load
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Save Data</a>
+                        <a class="dropdown-item" href="#">Load Data</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><b>Funds: {{ funds | currency }}</b></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
+import Portfolio from '../components/portfolio/Portfolio.vue'
+import Stocks from '../components/stocks/Stocks.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +11,17 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  }
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/stocks',
+    name: 'stocks',
+    component: Stocks
+  },
 ]
 
 const router = new VueRouter({
